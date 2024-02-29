d092d8e8c3e9a5cdcdb389503aa79ac2521e1004
Dastan104110 <abzeldastan@icloud.com>
2024-02-29 16:07:07 +0600

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Dastan on 29.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
